6b80d1e5d0bb843938685ffcf9e0ac3699b0831f
jjsanchezc <jjsanchezc@eafit.edu.co>
2026-08-09 17:37:48 -0500

    hyprlock ricing

--- hypr/colors.lua
+++ hypr/colors.lua
@@ -4,8 +4,8 @@
 hl.config({
     general = {
         col = {
-            active_border = "rgb(f5bd6f)",
-            inactive_border = "rgba(4f4539CC)",
+            active_border = "rgb(A7C080)",
+            inactive_border = "rgba(7A8478CC)",
         },
     },
 })
